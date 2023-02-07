b53c97b9ae5564c6fbc25b305bfee106e8f6ef54
clallain <clement.allain@inria.fr>
2023-02-07 09:19:03 +0100

    model1 -> model₁ ; model2 -> model₂

--- theories/std/counter.v
+++ theories/std/counter.v
@@ -80,9 +80,9 @@ Section counter_GS.
     meta l counter_meta_token γ_token ∗
     counter_token_frag γ_token n.
 
-  #[local] Definition counter_model1 γ_model n :=
+  #[local] Definition counter_model₁ γ_model n :=
     auth_excl_frag γ_model n.
-  #[local] Definition counter_model2 γ_model dq n :=
+  #[local] Definition counter_model₂ γ_model dq n :=
     auth_excl_auth γ_model dq n.
   Definition counter_model t dq n : iProp Σ :=
     ∃ l γ_mono γ_token γ_model,
@@ -92,12 +92,12 @@ Section counter_GS.
     meta l counter_meta_model γ_model ∗
     counter_mono_auth γ_mono dq n ∗
     counter_token_auth γ_token dq n ∗
-    counter_model2 γ_model dq n.
+    counter_model₂ γ_model dq n.
 
   #[local] Definition counter_inv_inner l γ_model : iProp Σ :=
     ∃ n,
     l ↦ #n ∗
-    counter_model1 γ_model n.
+    counter_model₁ γ_model n.
   Definition counter_inv t : iProp Σ :=
     ∃ l γ_model,
     ⌜t = #l⌝ ∗
@@ -134,15 +134,15 @@ Section counter_GS.
     Fractional (λ q, counter_model t (DfracOwn q) n).
   Proof.
     intros q1 q2. iSplit.
-    - iIntros "(%l & %γ_mono & %γ_token & %γ_model & -> & #Hmeta_mono & #Hmeta_token & #Hmeta_model & (Hmono_auth1 & Hmono_auth2) & (Htoken_auth1 & Htoken_auth2) & (Hmodel2_1 & Hmodel2_2))".
-      iSplitL "Hmono_auth1 Htoken_auth1 Hmodel2_1"; repeat iExists _; auto with iFrame.
-    - iIntros "((%_l & %_γ_mono & %_γ_token & %_γ_model & % & #Hmeta_mono & #Hmeta_token & #Hmeta_model & Hmono_auth1 & Htoken_auth1 & Hmodel2_1) & (%l & %γ_mono & %γ_token & %γ_model & % & #_Hmeta_mono & #_Hmeta_token & #_Hmeta_model & Hmono_auth2 & Htoken_auth2 & Hmodel2_2))". simplify.
+    - iIntros "(%l & %γ_mono & %γ_token & %γ_model & -> & #Hmeta_mono & #Hmeta_token & #Hmeta_model & (Hmono_auth1 & Hmono_auth2) & (Htoken_auth1 & Htoken_auth2) & (Hmodel₂1 & Hmodel₂2))".
+      iSplitL "Hmono_auth1 Htoken_auth1 Hmodel₂1"; repeat iExists _; auto with iFrame.
+    - iIntros "((%_l & %_γ_mono & %_γ_token & %_γ_model & % & #Hmeta_mono & #Hmeta_token & #Hmeta_model & Hmono_auth1 & Htoken_auth1 & Hmodel₂1) & (%l & %γ_mono & %γ_token & %γ_model & % & #_Hmeta_mono & #_Hmeta_token & #_Hmeta_model & Hmono_auth2 & Htoken_auth2 & Hmodel₂2))". simplify.
       iDestruct (meta_agree with "Hmeta_mono _Hmeta_mono") as %->. iClear "_Hmeta_mono".
       iDestruct (meta_agree with "Hmeta_token _Hmeta_token") as %->. iClear "_Hmeta_token".
       iDestruct (meta_agree with "Hmeta_model _Hmeta_model") as %->. iClear "_Hmeta_model".
       iCombine "Hmono_auth1 Hmono_auth2" as "Hmono_auth".
       iCombine "Htoken_auth1 Htoken_auth2" as "Htoken_auth".
-      iCombine "Hmodel2_1 Hmodel2_2" as "Hmodel2".
+      iCombine "Hmodel₂1 Hmodel₂2" as "Hmodel₂".
       repeat iExists _. auto with iFrame.
   Qed.
   #[global] Instance counter_model_as_fractional t q n :
@@ -156,7 +156,7 @@ Section counter_GS.
     counter_lb t m -∗
     ⌜m ≤ n⌝.
   Proof.
-    iIntros "(%_l & %_γ_mono & %γ_token & %γ_model & % & #Hmeta_mono & #Hmeta_token & #Hmeta_model & Hmono_auth & Htoken_auth & Hmodel2) (%l & %γ_mono & % & #_Hmeta_mono & Hmono_frag)". simplify.
+    iIntros "(%_l & %_γ_mono & %γ_token & %γ_model & % & #Hmeta_mono & #Hmeta_token & #Hmeta_model & Hmono_auth & Htoken_auth & Hmodel₂) (%l & %γ_mono & % & #_Hmeta_mono & Hmono_frag)". simplify.
     iDestruct (meta_agree with "Hmeta_mono _Hmeta_mono") as %->. iClear "_Hmeta_mono".
     iApply (auth_nat_max_both_valid with "Hmono_auth Hmono_frag").
   Qed.
@@ -173,7 +173,7 @@ Section counter_GS.
     counter_model t dq n -∗
     counter_lb t n.
   Proof.
-    iIntros "(%l & %γ_mono & %γ_token & %γ_model & % & #Hmeta_mono & #Hmeta_token & #Hmeta_model & Hmono_auth & Htoken_auth & Hmodel2)".
+    iIntros "(%l & %γ_mono & %γ_token & %γ_model & % & #Hmeta_mono & #Hmeta_token & #Hmeta_model & Hmono_auth & Htoken_auth & Hmodel₂)".
     iDestruct (auth_nat_max_frag_get with "Hmono_auth") as "#Hmono_frag".
     repeat iExists _. naive_solver.
   Qed.
@@ -183,7 +183,7 @@ Section counter_GS.
     counter_token t m -∗
     ⌜m < n⌝.
   Proof.
-    iIntros "(%_l & %γ_mono & %_γ_token & %γ_model & % & #Hmeta_mono & #Hmeta_token & #Hmeta_model & Hmono_auth & Htoken_auth & Hmodel2) (%l & %γ_token & % & #_Hmeta_token & Htoken_frag)". simplify.
+    iIntros "(%_l & %γ_mono & %_γ_token & %γ_model & % & #Hmeta_mono & #Hmeta_token & #Hmeta_model & Hmono_auth & Htoken_auth & Hmodel₂) (%l & %γ_token & % & #_Hmeta_token & Htoken_frag)". simplify.
     iDestruct (meta_agree with "Hmeta_token _Hmeta_token") as %->. iClear "_Hmeta_token".
     iDestruct (own_valid_2 with "Htoken_auth Htoken_frag") as %(_ & Hincluded & _)%auth_both_dfrac_valid_discrete.
     rewrite gset_disj_included -elem_of_subseteq_singleton elem_of_set_seq in Hincluded.
@@ -204,26 +204,26 @@ Section counter_GS.
     counter_model t dq n -∗
     ⌜✓ dq⌝.
   Proof.
-    iIntros "(%l & %γ_mono & %γ_token & %γ_model & -> & #Hmeta_mono & #Hmeta_token & #Hmeta_model & Hmono_auth & Htoken_auth & Hmodel2)".
-    iApply (auth_excl_auth_valid with "Hmodel2").
+    iIntros "(%l & %γ_mono & %γ_token & %γ_model & -> & #Hmeta_mono & #Hmeta_token & #Hmeta_model & Hmono_auth & Htoken_auth & Hmodel₂)".
+    iApply (auth_excl_auth_valid with "Hmodel₂").
   Qed.
   Lemma counter_model_valid_2 t dq1 n1 dq2 n2 :
     counter_model t dq1 n1 -∗
     counter_model t dq2 n2 -∗
     ⌜✓ (dq1 ⋅ dq2) ∧ n1 = n2⌝.
   Proof.
-    iIntros "(%_l & %_γ_mono & %_γ_token & %_γ_model & % & #Hmeta_mono & #Hmeta_token & #Hmeta_model & Hmono_auth1 & Htoken_auth1 & Hmodel2_1) (%l & %γ_mono & %γ_token & %γ_model & % & #_Hmeta_mono & #_Hmeta_token & #_Hmeta_model & Hmono_auth2 & Htoken_auth2 & Hmodel2_2)". simplify.
+    iIntros "(%_l & %_γ_mono & %_γ_token & %_γ_model & % & #Hmeta_mono & #Hmeta_token & #Hmeta_model & Hmono_auth1 & Htoken_auth1 & Hmodel₂1) (%l & %γ_mono & %γ_token & %γ_model & % & #_Hmeta_mono & #_Hmeta_token & #_Hmeta_model & Hmono_auth2 & Htoken_auth2 & Hmodel₂2)". simplify.
     iDestruct (meta_agree with "Hmeta_model _Hmeta_model") as %->. iClear "_Hmeta_model".
-    iApply (auth_excl_auth_valid_2 with "Hmodel2_1 Hmodel2_2").
+    iApply (auth_excl_auth_valid_2 with "Hmodel₂1 Hmodel₂2").
   Qed.
   Lemma counter_model_persist t dq n :
     counter_model t dq n ==∗
     counter_model t DfracDiscarded n.
   Proof.
-    iIntros "(%l & %γ_mono & %γ_token & %γ_model & -> & #Hmeta_mono & #Hmeta_token & #Hmeta_model & Hmono_auth & Htoken_auth & Hmodel2)".
+    iIntros "(%l & %γ_mono & %γ_token & %γ_model & -> & #Hmeta_mono & #Hmeta_token & #Hmeta_model & Hmono_auth & Htoken_auth & Hmodel₂)".
     iMod (auth_nat_max_auth_persist with "Hmono_auth") as "Hmono_auth".
     iMod (own_update with "Htoken_auth") as "Htoken_auth"; first eapply auth_update_auth_persist.
-    iMod (auth_excl_auth_persist with "Hmodel2") as "Hmodel2".
+    iMod (auth_excl_auth_persist with "Hmodel₂") as "Hmodel₂".
     repeat iExists _. iFrame "∗#". done.
   Qed.
   Lemma counter_model_exclusive t n1 n2 :
@@ -231,9 +231,9 @@ Section counter_GS.
     counter_model t (DfracOwn 1) n2 -∗
     False.
   Proof.
-    iIntros "(%_l & %_γ_mono & %_γ_token & %_γ_model & % & #Hmeta_mono & #Hmeta_token & #Hmeta_model & Hmono_auth1 & Htoken_auth1 & Hmodel2_1) (%l & %γ_mono & %γ_token & %γ_model & % & #_Hmeta_mono & #_Hmeta_token & #_Hmeta_model & Hmono_auth2 & Htoken_auth2 & Hmodel2_2)". simplify.
+    iIntros "(%_l & %_γ_mono & %_γ_token & %_γ_model & % & #Hmeta_mono & #Hmeta_token & #Hmeta_model & Hmono_auth1 & Htoken_auth1 & Hmodel₂1) (%l & %γ_mono & %γ_token & %γ_model & % & #_Hmeta_mono & #_Hmeta_token & #_Hmeta_model & Hmono_auth2 & Htoken_auth2 & Hmodel₂2)". simplify.
     iDestruct (meta_agree with "Hmeta_model _Hmeta_model") as %->. iClear "_Hmeta_model".
-    iApply (auth_excl_auth_exclusive with "Hmodel2_1 Hmodel2_2").
+    iApply (auth_excl_auth_exclusive with "Hmodel₂1 Hmodel₂2").
   Qed.
 
   Lemma counter_make_spec :
@@ -245,14 +245,14 @@ Section counter_GS.
     wp_rec. iApply wp_fupd. wp_apply (wp_alloc with "[//]"). iIntros "%l (Hl & Hmeta)".
     iMod (auth_nat_max_alloc 0) as "(%γ_mono & Hmono_auth & _)".
     iMod (own_alloc (● GSet ∅)) as "(%γ_token & Htoken_auth)"; first by apply auth_auth_valid.
-    iMod (auth_excl_alloc 0) as "(%γ_model & Hmodel2 & Hmodel1)"; first done.
+    iMod (auth_excl_alloc 0) as "(%γ_model & Hmodel₂ & Hmodel₁)"; first done.
     iDestruct (meta_token_difference _ (↑ counter_meta_mono) with "Hmeta") as "(Hmeta_mono & Hmeta)"; first solve_ndisj.
     iDestruct (meta_token_difference _ (↑ counter_meta_token) with "Hmeta") as "(Hmeta_token & Hmeta)"; first solve_ndisj.
     iDestruct (meta_token_difference _ (↑ counter_meta_model) with "Hmeta") as "(Hmeta_model & Hmeta)"; first solve_ndisj.
     iMod (meta_set _ _ γ_mono with "Hmeta_mono") as "#Hmeta_mono"; first done.
     iMod (meta_set _ _ γ_token with "Hmeta_token") as "#Hmeta_token"; first done.
     iMod (meta_set _ _ γ_model with "Hmeta_model") as "#Hmeta_model"; first done.
-    iApply "HΦ". iSplitL "Hl Hmodel1".
+    iApply "HΦ". iSplitL "Hl Hmodel₁".
     - iExists l, γ_model. repeat iSplitR; [done.. |].
       iApply inv_alloc. iExists 0. iFrame.
     - iExists l, γ_mono, γ_token, γ_model. iFrame "∗#". done.
@@ -266,21 +266,21 @@ Section counter_GS.
   Proof.
     iIntros "!> %Φ (%l & %γ_model & -> & #Hmeta_model & #Hinv) HΦ".
     wp_rec.
-    iInv "Hinv" as "(%n & Hl & Hmodel1)".
+    iInv "Hinv" as "(%n & Hl & Hmodel₁)".
     wp_faa.
-    iMod "HΦ" as "(%_n & (%_l & %γ_mono & %γ_token & %_γ_model & %Heq & #Hmeta_mono & #Hmeta_token & #_Hmeta_model & Hmono_auth & Htoken_auth & Hmodel2) & _ & HΦ)". injection Heq as <-.
+    iMod "HΦ" as "(%_n & (%_l & %γ_mono & %γ_token & %_γ_model & %Heq & #Hmeta_mono & #Hmeta_token & #_Hmeta_model & Hmono_auth & Htoken_auth & Hmodel₂) & _ & HΦ)". injection Heq as <-.
     iDestruct (meta_agree with "Hmeta_model _Hmeta_model") as %<-. iClear "_Hmeta_model".
-    iDestruct (auth_excl_both_valid_L with "Hmodel2 Hmodel1") as %->.
+    iDestruct (auth_excl_both_valid_L with "Hmodel₂ Hmodel₁") as %->.
     iMod (auth_nat_max_update (S n) with "Hmono_auth") as "Hmono_auth"; first lia.
     iMod (own_update _ _ (● GSet (set_seq 0 (S n)) ⋅ ◯ GSet {[n]}) with "Htoken_auth") as "(Htoken_auth & Htoken_frag)".
     { apply auth_update_alloc.
       rewrite set_seq_S_end_union_L. apply gset_disj_alloc_empty_local_update.
       rewrite -{1}(Nat.add_0_l n). apply set_seq_S_end_disjoint.
     }
-    iMod (auth_excl_update' (S n) with "Hmodel2 Hmodel1") as "(Hmodel2 & Hmodel1)".
+    iMod (auth_excl_update' (S n) with "Hmodel₂ Hmodel₁") as "(Hmodel₂ & Hmodel₁)".
     iAssert (counter_token #l n) with "[Htoken_frag]" as "Htoken".
     { iExists l, γ_token. naive_solver. }
-    iAssert (counter_model #l (DfracOwn 1) (S n)) with "[Hmono_auth Htoken_auth Hmodel2]" as "Hmodel".
+    iAssert (counter_model #l (DfracOwn 1) (S n)) with "[Hmono_auth Htoken_auth Hmodel₂]" as "Hmodel".
     { iExists l, γ_mono, γ_token, γ_model. iFrame "∗#". done. }
     iMod ("HΦ" with "[$Hmodel $Htoken] [//]") as "$".
     iModIntro. iExists (S n). iNext. iFrame.
@@ -295,12 +295,12 @@ Section counter_GS.
   Proof.
     iIntros "!> %Φ (%l & %γ_model & -> & #Hmeta_model & #Hinv) HΦ".
     wp_rec.
-    iInv "Hinv" as "(%n & Hl & Hmodel1)".
+    iInv "Hinv" as "(%n & Hl & Hmodel₁)".
     wp_load.
-    iMod "HΦ" as "(%dq & %_n & (%_l & %γ_mono & %γ_token & %_γ_model & %Heq & #Hmeta_mono & #Hmeta_token & #_Hmeta_model & Hmono_auth & Htoken_auth & Hmodel2) & _ & HΦ)". injection Heq as <-.
+    iMod "HΦ" as "(%dq & %_n & (%_l & %γ_mono & %γ_token & %_γ_model & %Heq & #Hmeta_mono & #Hmeta_token & #_Hmeta_model & Hmono_auth & Htoken_auth & Hmodel₂) & _ & HΦ)". injection Heq as <-.
     iDestruct (meta_agree with "Hmeta_model _Hmeta_model") as %<-. iClear "_Hmeta_model".
-    iDestruct (auth_excl_both_valid_L with "Hmodel2 Hmodel1") as %->.
-    iAssert (counter_model #l dq n) with "[Hmono_auth Htoken_auth Hmodel2]" as "Hmodel".
+    iDestruct (auth_excl_both_valid_L with "Hmodel₂ Hmodel₁") as %->.
+    iAssert (counter_model #l dq n) with "[Hmono_auth Htoken_auth Hmodel₂]" as "Hmodel".
     { iExists l, γ_mono, γ_token, γ_model. iFrame "∗#". done. }
     iMod ("HΦ" with "Hmodel [//]") as "$".
     iModIntro. iNext. iExists n. iFrame.
