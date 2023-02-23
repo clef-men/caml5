3e2f8857f724d7473d70a53b5e155e8686919860
clallain <clement.allain@inria.fr>
2023-02-23 22:48:17 +0100

    tail --> sentinel

--- theories/std/queue.v
+++ theories/std/queue.v
@@ -50,39 +50,39 @@ Record queue `{!heapGS Σ} := {
 Section std_queue.
   Context `{!heapGS Σ}.
   Implicit Types l : loc.
-  Implicit Types t v hd tl : val.
+  Implicit Types t v hd sent : val.
 
   Notation queue_offset_head := 0%Z.
-  Notation queue_offset_tail := 1%Z.
+  Notation queue_offset_sentinel := 1%Z.
 
   Notation "t '.(head)'" := (t +ₗ queue_offset_head)%stdpp
   ( at level 5
   ) : stdpp_scope.
-  Notation "t '.(tail)'" := (t +ₗ queue_offset_tail)%stdpp
+  Notation "t '.(sentinel)'" := (t +ₗ queue_offset_sentinel)%stdpp
   ( at level 5
   ) : stdpp_scope.
   Notation "t '.(head)'" := (t +ₗ #queue_offset_head)%E
   ( at level 5
   ) : expr_scope.
-  Notation "t '.(tail)'" := (t +ₗ #queue_offset_tail)%E
+  Notation "t '.(sentinel)'" := (t +ₗ #queue_offset_sentinel)%E
   ( at level 5
   ) : expr_scope.
 
   Definition std_queue_make : val :=
     λ: <>,
-      let: "tl" := chain_cons #() #() in
-      AllocN #2 "tl".
+      let: "sent" := chain_cons #() #() in
+      AllocN #2 "sent".
 
   Definition std_queue_is_empty : val :=
     λ: "t",
-      !"t".(head) = !"t".(tail).
+      !"t".(head) = !"t".(sentinel).
 
   Definition std_queue_push : val :=
     λ: "t" "v",
-      let: "tl" := chain_cons #() #() in
-      chain_set_head !"t".(tail) "v" ;;
-      chain_set_tail !"t".(tail) "tl" ;;
-      "t".(tail) <- "tl".
+      let: "sent" := chain_cons #() #() in
+      chain_set_head !"t".(sentinel) "v" ;;
+      chain_set_tail !"t".(sentinel) "sent" ;;
+      "t".(sentinel) <- "sent".
 
   Definition std_queue_pop : val :=
     λ: "t",
@@ -95,10 +95,10 @@ Section std_queue.
       ).
 
   Definition std_queue_model t vs : iProp Σ :=
-    ∃ l hd tl,
+    ∃ l hd sent,
     ⌜t = #l⌝ ∗
-    l.(head) ↦ hd ∗ l.(tail) ↦ tl ∗
-    chain_model hd (DfracOwn 1) (reverse vs) tl ∗ chain_model tl (DfracOwn 1) [ #()] #().
+    l.(head) ↦ hd ∗ l.(sentinel) ↦ sent ∗
+    chain_model hd (DfracOwn 1) (reverse vs) sent ∗ chain_model sent (DfracOwn 1) [ #()] #().
 
   #[global] Instance std_queue_model_timeless t vs :
     Timeless (std_queue_model t vs).
@@ -113,12 +113,12 @@ Section std_queue.
   Proof.
     iIntros "%Φ _ HΦ".
     wp_rec.
-    wp_apply (chain_cons_spec _ (DfracOwn 1)); [done | by iApply chain_model_nil |]. iIntros "%tl Htl_model".
+    wp_apply (chain_cons_spec _ (DfracOwn 1)); [done | by iApply chain_model_nil |]. iIntros "%sent Hsent_model".
     wp_pures. wp_alloc l as "Hl"; first done.
-    iDestruct (array_cons with "Hl") as "(Hhd & Htl)".
+    iDestruct (array_cons with "Hl") as "(Hhd & Hsent)".
     iEval (setoid_rewrite <- loc_add_0) in "Hhd".
-    iDestruct (array_singleton with "Htl") as "Htl".
-    iModIntro. iApply "HΦ". iExists l, tl, tl. iFrame. iSplit; [done | by iApply chain_model_nil].
+    iDestruct (array_singleton with "Hsent") as "Hsent".
+    iModIntro. iApply "HΦ". iExists l, sent, sent. iFrame. iSplit; [done | by iApply chain_model_nil].
   Qed.
 
   Lemma std_queue_is_empty_spec t vs :
@@ -126,18 +126,18 @@ Section std_queue.
       std_queue_is_empty t
     {{{ RET #(bool_decide (vs = [])); std_queue_model t vs }}}.
   Proof.
-    iIntros "%Φ (%l & %hd & %tl & -> & Hhd & Htl & Hhd_model & Htl_model) HΦ".
+    iIntros "%Φ (%l & %hd & %sent & -> & Hhd & Hsent & Hhd_model & Hsent_model) HΦ".
     wp_rec. wp_pures. wp_load. wp_pures. wp_load.
     destruct vs as [| v vs].
     - iDestruct (chain_model_nil with "Hhd_model") as %->.
-      iDestruct (chain_model_unboxed with "Htl_model") as %?.
+      iDestruct (chain_model_unboxed with "Hsent_model") as %?.
       wp_pures.
-      iEval (rewrite bool_decide_eq_true_2). iApply "HΦ". iExists l, tl, tl. auto with iFrame.
-    - iDestruct (chain_model_unboxed with "Htl_model") as %?.
+      iEval (rewrite bool_decide_eq_true_2). iApply "HΦ". iExists l, sent, sent. auto with iFrame.
+    - iDestruct (chain_model_unboxed with "Hsent_model") as %?.
       wp_pures.
-      iDestruct (chain_model_ne with "Htl_model Hhd_model") as %?; first (rewrite reverse_length /=; lia).
-      rewrite (bool_decide_eq_false_2 (hd = tl)); last done.
-      iApply "HΦ". iExists l, hd, tl. auto with iFrame.
+      iDestruct (chain_model_ne with "Hsent_model Hhd_model") as %?; first (rewrite reverse_length /=; lia).
+      rewrite (bool_decide_eq_false_2 (hd = sent)); last done.
+      iApply "HΦ". iExists l, hd, sent. auto with iFrame.
   Qed.
 
   Lemma std_queue_push_spec t vs v :
@@ -145,17 +145,17 @@ Section std_queue.
       std_queue_push t v
     {{{ RET #(); std_queue_model t (v :: vs) }}}.
   Proof.
-    iIntros "%Φ (%l & %hd & %tl & -> & Hhd & Htl & Hhd_model & Htl_model) HΦ".
+    iIntros "%Φ (%l & %hd & %sent & -> & Hhd & Hsent & Hhd_model & Hsent_model) HΦ".
     wp_rec. wp_pures.
-    wp_apply (chain_cons_spec _ (DfracOwn 1)); [done | by iApply chain_model_nil |]. iIntros "%tl' Htl'_model".
+    wp_apply (chain_cons_spec _ (DfracOwn 1)); [done | by iApply chain_model_nil |]. iIntros "%sent' Hsent'_model".
     wp_pures. wp_load.
-    wp_apply (chain_set_head_spec with "Htl_model"). iIntros "Htl_model".
+    wp_apply (chain_set_head_spec with "Hsent_model"). iIntros "Hsent_model".
     wp_pures. wp_load.
-    wp_apply (chain_set_tail_spec with "Htl_model"). iIntros "%_ (Htl_model & _)".
-    iDestruct (chain_model_app_1 with "Hhd_model Htl_model") as "Hhd_model".
+    wp_apply (chain_set_tail_spec with "Hsent_model"). iIntros "%_ (Hsent_model & _)".
+    iDestruct (chain_model_app_1 with "Hhd_model Hsent_model") as "Hhd_model".
     rewrite -reverse_cons.
     wp_pures. wp_store.
-    iModIntro. iApply "HΦ". iExists l, hd, tl'. auto with iFrame.
+    iModIntro. iApply "HΦ". iExists l, hd, sent'. auto with iFrame.
   Qed.
 
   Lemma std_queue_pop_spec t vs :
@@ -169,11 +169,11 @@ Section std_queue.
   Proof.
     iIntros "%Φ Hmodel HΦ".
     wp_rec.
-    wp_apply (std_queue_is_empty_spec with "Hmodel"). iIntros "(%l & %hd & %tl & -> & Hhd & Htl & Hhd_model & Htl_model)".
+    wp_apply (std_queue_is_empty_spec with "Hmodel"). iIntros "(%l & %hd & %sent & -> & Hhd & Hsent & Hhd_model & Hsent_model)".
     destruct (reverse vs) as [| v rev_vs] eqn:Heq;
       apply (f_equal reverse) in Heq; rewrite reverse_involutive in Heq; subst;
       wp_pures.
-    { iModIntro. iApply "HΦ". iLeft. iSplit; first done. iExists l, hd, tl. auto with iFrame. }
+    { iModIntro. iApply "HΦ". iLeft. iSplit; first done. iExists l, hd, sent. auto with iFrame. }
     rewrite reverse_cons bool_decide_eq_false_2; last eauto using app_cons_not_nil.
     wp_pures. wp_load.
     wp_apply (chain_head_spec with "Hhd_model"). iIntros "Hhd_model".
@@ -181,7 +181,7 @@ Section std_queue.
     wp_apply (chain_tail_spec with "Hhd_model"). iIntros "%hd' (Hhd_model & Hhd'_model)".
     wp_store. wp_pures.
     iModIntro. iApply "HΦ". iRight. iExists (reverse rev_vs), v. iSplit; first done.
-    iExists l, hd', tl. rewrite reverse_involutive. auto with iFrame.
+    iExists l, hd', sent. rewrite reverse_involutive. auto with iFrame.
   Qed.
 
   Definition std_queue : queue Σ :=
