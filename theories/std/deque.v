a7d6261b19b9c549cb62720e054996c3ca35a6a2
clallain <clement.allain@inria.fr>
2023-03-01 00:52:15 +0100

    Add std/dlchain. Begin std/deque.std_deque.

--- theories/std/deque.v
+++ theories/std/deque.v
@@ -1,9 +1,12 @@
 From caml5 Require Import
   prelude.
 From caml5.lang Require Import
-  notations.
+  notations
+  proofmode.
 From caml5.std Require Export
   base.
+From caml5.std Require Import
+  dlchain.
 
 Record deque `{!heapGS Σ} := {
   deque_make : val ;
@@ -59,3 +62,156 @@ Record deque `{!heapGS Σ} := {
 #[global] Arguments deque _ {_} : assert.
 #[global] Arguments Build_deque {_ _ _ _ _ _ _ _ _ _} _ _ _ _ _ _ : assert.
 #[global] Existing Instance deque_model_timeless.
+
+Section std_deque.
+  Context `{!heapGS Σ}.
+
+  Definition std_deque_make : val :=
+    λ: <>,
+      let: "t" := dlchain_singleton #() #() #() in
+      dlchain_set_prev "t" "t" ;;
+      dlchain_set_next "t" "t" ;;
+      "t".
+
+  Definition std_deque_is_empty : val :=
+    λ: "t",
+      dlchain_next "t" = "t".
+
+  Definition std_deque_push_front : val :=
+    λ: "t" "v",
+      let: "old_front" := dlchain_next "t" in
+      let: "front" := dlchain_singleton "t" "v" "old_front" in
+      dlchain_set_next "t" "front" ;;
+      dlchain_set_prev "old_front" "front".
+
+  Definition std_deque_pop_front : val :=
+    λ: "t",
+      if: std_deque_is_empty "t" then (
+        NONE
+      ) else (
+        let: "old_front" := dlchain_next "t" in
+        let: "front" := dlchain_next "old_front" in
+        dlchain_set_next "t" "front" ;;
+        dlchain_set_prev "front" "t" ;;
+        SOME (dlchain_data "old_front")
+      ).
+
+  Definition std_deque_push_back : val :=
+    λ: "t" "v",
+      let: "old_back" := dlchain_prev "t" in
+      let: "back" := dlchain_singleton "old_back" "v" "t" in
+      dlchain_set_next "old_back" "back" ;;
+      dlchain_set_prev "t" "back".
+
+  Definition std_deque_pop_back : val :=
+    λ: "t",
+      if: std_deque_is_empty "t" then (
+        NONE
+      ) else (
+        let: "old_back" := dlchain_prev "t" in
+        let: "back" := dlchain_prev "old_back" in
+        dlchain_set_next "old_back" "back" ;;
+        dlchain_set_prev "t" "back" ;;
+        SOME (dlchain_data "old_back")
+      ).
+
+  Definition std_deque_model t vs : iProp Σ :=
+    ∃ back, dlchain_model t (DfracOwn 1) back (#() :: vs) back t.
+
+  #[global] Instance std_deque_model_timeless t vs :
+    Timeless (std_deque_model t vs).
+  Proof.
+    apply _.
+  Qed.
+
+  Lemma std_deque_make_spec :
+    {{{ True }}}
+      std_deque_make #()
+    {{{ t, RET t; std_deque_model t [] }}}.
+  Proof.
+    iIntros "%Φ _ HΦ".
+    wp_rec.
+    wp_apply (dlchain_singleton_spec with "[//]"). iIntros "%sent Hsent".
+    wp_pures.
+    wp_apply (dlchain_set_prev_spec with "Hsent"); first (simpl; lia). iIntros "Hsent".
+    wp_pures.
+    wp_apply (dlchain_set_next_spec with "Hsent"). iIntros "%_ (Hsent & _)".
+    wp_pures.
+    iApply "HΦ". iExists sent. auto with iFrame.
+  Qed.
+
+  Lemma std_deque_is_empty_spec t vs :
+    {{{ std_deque_model t vs }}}
+      std_deque_is_empty t
+    {{{ RET #(bool_decide (vs = [])); std_deque_model t vs }}}.
+  Proof.
+    iIntros "%Φ (%back & Hsent) HΦ".
+    wp_rec.
+    wp_apply (dlchain_next_spec with "Hsent"). iIntros "%front (Hsent & Hfront)".
+    iDestruct (dlchain_model_unboxed with "Hsent") as %Hunboxed. wp_pures. clear Hunboxed.
+    destruct vs as [| v vs].
+    - iDestruct (dlchain_model_nil_2 with "Hfront") as %(-> & ->).
+      rewrite /= bool_decide_eq_true_2; last done.
+      iApply "HΦ". iExists back. auto with iFrame.
+    - iAssert ⌜t ≠ front⌝%I as %?.
+      { admit.
+      }
+      rewrite /= bool_decide_eq_false_2; last done.
+      iDestruct (dlchain_model_app_1 with "Hsent Hfront") as "Hsent".
+      iApply "HΦ". iExists back. auto with iFrame.
+  Admitted.
+
+  Lemma std_deque_push_front_spec t vs v :
+    {{{ std_deque_model t vs }}}
+      std_deque_push_front t v
+    {{{ RET #(); std_deque_model t (v :: vs) }}}.
+  Proof.
+  Admitted.
+
+  Lemma std_deque_pop_front_spec t vs :
+    {{{ std_deque_model t vs }}}
+      std_deque_pop_front t
+    {{{ w,
+      RET w;
+      (⌜vs = [] ∧ w = NONEV⌝ ∗ std_deque_model t []) ∨
+      (∃ v vs', ⌜vs = v :: vs' ∧ w = SOMEV v⌝ ∗ std_deque_model t vs')
+    }}}.
+  Proof.
+  Admitted.
+
+  Lemma std_deque_push_back_spec t vs v :
+    {{{ std_deque_model t vs }}}
+      std_deque_push_back t v
+    {{{ RET #(); std_deque_model t (vs ++ [v]) }}}.
+  Proof.
+  Admitted.
+
+  Lemma std_deque_pop_back_spec t vs :
+    {{{ std_deque_model t vs }}}
+      std_deque_pop_back t
+    {{{ w,
+      RET w;
+      (⌜vs = [] ∧ w = NONEV⌝ ∗ std_deque_model t []) ∨
+      (∃ vs' v, ⌜vs = vs' ++ [v] ∧ w = SOMEV v⌝ ∗ std_deque_model t vs')
+    }}}.
+  Proof.
+  Admitted.
+
+  Definition std_deque :=
+    Build_deque
+      std_deque_make_spec
+      std_deque_is_empty_spec
+      std_deque_push_front_spec
+      std_deque_pop_front_spec
+      std_deque_push_back_spec
+      std_deque_pop_back_spec.
+End std_deque.
+
+#[global] Opaque std_deque_make.
+#[global] Opaque std_deque_is_empty.
+#[global] Opaque std_deque_push_front.
+#[global] Opaque std_deque_pop_front.
+#[global] Opaque std_deque_push_back.
+#[global] Opaque std_deque_pop_back.
+
+#[global] Opaque std_deque_model.
---
+++ theories/std/dlchain.v
@@ -0,0 +1,404 @@
+From caml5 Require Import
+  prelude.
+From caml5.lang Require Import
+  notations
+  proofmode.
+From caml5.std Require Export
+  base.
+From caml5.std Require Import
+  record_three.
+
+Section heapGS.
+  Context `{!heapGS Σ}.
+  Implicit Types l : loc.
+  Implicit Types v w t prev last next : val.
+
+  Notation "t '.(prev)'" := (t.0)%E
+  ( at level 5
+  ) : expr_scope.
+  Notation "t '.(data)'" := (t.1)%E
+  ( at level 5
+  ) : expr_scope.
+  Notation "t '.(next)'" := (t.2)%E
+  ( at level 5
+  ) : expr_scope.
+
+  Definition dlchain_prev : val :=
+    λ: "t",
+      !"t".(prev).
+  Definition dlchain_data : val :=
+    λ: "t",
+      !"t".(data).
+  Definition dlchain_next : val :=
+    λ: "t",
+      !"t".(next).
+
+  Definition dlchain_set_prev : val :=
+    λ: "t" "prev",
+      "t".(prev) <- "prev".
+  Definition dlchain_set_data : val :=
+    λ: "t" "data",
+      "t".(data) <- "data".
+  Definition dlchain_set_next : val :=
+    λ: "t" "next",
+      "t".(next) <- "next".
+
+  Definition dlchain_singleton : val :=
+    λ: "prev" "v" "next",
+      record_three_make "prev" "v" "next".
+
+  Definition dlchain_advance_backward : val :=
+    rec: "dlchain_advance_backward" "t" "i" :=
+      if: "i" = #0 then (
+        "t"
+      ) else (
+      "dlchain_advance_backward" (dlchain_prev "t") ("i" - #1)
+    ).
+  Definition dlchain_advance_forward : val :=
+    rec: "dlchain_advance_forward" "t" "i" :=
+      if: "i" = #0 then (
+        "t"
+      ) else (
+      "dlchain_advance_forward" (dlchain_next "t") ("i" - #1)
+    ).
+  Definition dlchain_advance : val :=
+    λ: "t" "i",
+      if: "i" < #0 then (
+        dlchain_advance_backward "t" "i"
+      ) else (
+        dlchain_advance_forward "t" "i"
+      ).
+
+  Definition dlchain_get : val :=
+    λ: "t" "i",
+      dlchain_data (dlchain_advance "t" "i").
+  Definition dlchain_set : val :=
+    λ: "t" "i" "v",
+      dlchain_set_data (dlchain_advance "t" "i") "v".
+
+  #[local] Definition dlchain_cell l dq prev v next :=
+    record_three_model l dq prev v next.
+  Fixpoint dlchain_model t dq prev vs last next : iProp Σ :=
+    match vs with
+    | [] =>
+        ⌜t = next ∧ prev = last⌝
+    | v :: vs =>
+        ∃ l t',
+        ⌜t = #l⌝ ∗
+        dlchain_cell l dq prev v t' ∗ dlchain_model t' dq t vs last next
+    end.
+
+  Lemma dlchain_model_unboxed t dq prev v vs last next :
+    dlchain_model t dq prev (v :: vs) last next -∗
+    ⌜val_is_unboxed t⌝.
+  Proof.
+    iIntros "(%l & %_ & -> & _) //".
+  Qed.
+
+  #[global] Instance dlchain_model_timeless t dq prev vs last next :
+    Timeless (dlchain_model t dq prev vs last next).
+  Proof.
+    revert t prev. induction vs; apply _.
+  Qed.
+  #[global] Instance dlchain_model_persistent t prev vs last next :
+    Persistent (dlchain_model t DfracDiscarded prev vs last next).
+  Proof.
+    rewrite /Persistent.
+    iInduction vs as [| v vs] "IH" forall (t prev); first naive_solver.
+    iIntros "(%l & %t' & -> & #Hcell & Hmodel')".
+    iDestruct ("IH" with "Hmodel'") as "#?".
+    iModIntro. naive_solver.
+  Qed.
+
+  #[global] Instance dlchain_model_fractional t prev vs last next :
+    Fractional (λ q, dlchain_model t (DfracOwn q) prev vs last next).
+  Proof.
+    intros q1 q2. iSplit.
+    - iInduction vs as [| v vs] "IH" forall (t prev); first done.
+      iIntros "(%l & %t' & -> & Hcell & Hmodel')".
+      iDestruct (fractional_split_1 with "Hcell") as "(Hcell1 & Hcell2)".
+      iDestruct ("IH" with "Hmodel'") as "(Hmodel'1 & Hmodel'2)".
+      iSplitL "Hcell1 Hmodel'1"; repeat iExists _; auto with iFrame.
+    - iInduction vs as [| v vs] "IH" forall (t prev); first iIntros "(% & _) //".
+      iIntros "((%l & %t' & -> & Hcell1 & Hmodel'1) & (%_l & %_t' & %Heq & Hcell2 & Hmodel'2))". injection Heq as <-.
+      iDestruct (record_three_model_agree with "Hcell1 Hcell2") as %(_ & _ & <-).
+      iDestruct (fractional_merge with "Hcell1 Hcell2") as "Hcell".
+      iDestruct ("IH" with "[$Hmodel'1 $Hmodel'2]") as "Hmodel'".
+      repeat iExists _. auto with iFrame.
+  Qed.
+  #[global] Instance dlchain_model_as_fractional t q prev vs last next :
+    AsFractional (dlchain_model t (DfracOwn q) prev vs last next) (λ q, dlchain_model t (DfracOwn q) prev vs last next) q.
+  Proof.
+    split; [done | apply _].
+  Qed.
+
+  Lemma dlchain_model_nil t dq prev last next :
+    ⌜t = next ∧ prev = last⌝ ⊣⊢
+    dlchain_model t dq prev [] last next.
+  Proof.
+    auto.
+  Qed.
+  Lemma dlchain_model_nil_1 v prev dq :
+    ⊢ dlchain_model v dq prev [] prev v.
+  Proof.
+    auto.
+  Qed.
+  Lemma dlchain_model_nil_2 t dq prev last next :
+    dlchain_model t dq prev [] last next -∗
+    ⌜t = next ∧ prev = last⌝.
+  Proof.
+    auto.
+  Qed.
+
+  Lemma dlchain_model_app_1 dq t1 prev vs1 last1 t2 vs2 last2 next :
+    dlchain_model t1 dq prev vs1 last1 t2 -∗
+    dlchain_model t2 dq last1 vs2 last2 next -∗
+    dlchain_model t1 dq prev (vs1 ++ vs2) last2 next.
+  Proof.
+    iInduction vs1 as [| v1 vs1] "IH" forall (t1 prev).
+    - iIntros "(-> & ->)". naive_solver.
+    - iIntros "(%l1 & %t1' & -> & Hcell1 & Hmodel1') Hmodel2".
+      rewrite -app_comm_cons.
+      iExists l1, t1'. iFrame. iSplit; first done.
+      iApply ("IH" with "Hmodel1' Hmodel2").
+  Qed.
+  Lemma dlchain_model_app_2 vs1 vs2 t dq prev vs last next :
+    vs = vs1 ++ vs2 →
+    dlchain_model t dq prev vs last next -∗
+      ∃ last' t',
+      dlchain_model t dq prev vs1 last' t' ∗
+      dlchain_model t' dq last' vs2 last next.
+  Proof.
+    iInduction vs1 as [| v1 vs1] "IH" forall (t prev vs); iIntros (->).
+    - naive_solver.
+    - rewrite -app_comm_cons.
+      iIntros "(%l & %t' & -> & Hcell & Hmodel')".
+      iDestruct ("IH" with "[//] Hmodel'") as "(%last' & %t'' & Hmodel' & Hmodel'')".
+      iExists last', t''. iFrame. iExists l, t'. auto with iFrame.
+  Qed.
+  Lemma dlchain_model_app t dq prev vs1 vs2 last next :
+    ( ∃ last' t',
+      dlchain_model t dq prev vs1 last' t' ∗
+      dlchain_model t' dq last' vs2 last next
+    ) ⊣⊢
+    dlchain_model t dq prev (vs1 ++ vs2) last next.
+  Proof.
+    iSplit.
+    - iIntros "(%last' & %t' & Hmodel & Hmodel')".
+      iApply (dlchain_model_app_1 with "Hmodel Hmodel'").
+    - iApply dlchain_model_app_2; first done.
+  Qed.
+
+  Lemma dlchain_model_persist t dq prev vs last next :
+    dlchain_model t dq prev vs last next ==∗
+    dlchain_model t DfracDiscarded prev vs last next.
+  Proof.
+    iInduction vs as [| v vs] "IH" forall (t prev); first done.
+    iIntros "(%l & %t' & -> & Hcell & Hmodel')".
+    iMod (record_three_model_persist with "Hcell") as "Hcell".
+    iMod ("IH" with "Hmodel'") as "Hmodel'".
+    repeat iExists _. naive_solver.
+  Qed.
+
+  Lemma dlchain_model_valid t dq prev vs last next :
+    0 < length vs →
+    dlchain_model t dq prev vs last next -∗
+    ⌜✓ dq⌝.
+  Proof.
+    intros. destruct vs as [| v vs]; first naive_solver lia.
+    iIntros "(%l & %t' & -> & Hcell & Hmodel')".
+    iApply (record_three_model_valid with "Hcell").
+  Qed.
+  Lemma dlchain_model_combine t prev dq1 vs1 last1 next1 dq2 vs2 last2 next2 :
+    length vs1 ≤ length vs2 →
+    dlchain_model t dq1 prev vs1 last1 next1 -∗
+    dlchain_model t dq2 prev vs2 last2 next2 -∗
+      dlchain_model t (dq1 ⋅ dq2) prev vs1 last1 next1 ∗
+      dlchain_model next1 dq2 last1 (drop (length vs1) vs2) last2 next2 ∗
+      ⌜vs1 = take (length vs1) vs2⌝.
+  Proof.
+    iInduction vs1 as [| v1 vs1] "IH" forall (t prev vs2);
+      destruct vs2 as [| v2 vs2].
+    - iIntros "_ (-> & ->) (-> & ->) //".
+    - iIntros "_ (-> & ->) (%l & %t' & -> & Hcell & Hmodel')".
+      repeat iSplit; try done. iExists l, t'. auto with iFrame.
+    - iIntros "%Hlength". simpl in Hlength. lia.
+    - iIntros "%Hlength (%l & %t' & -> & Hcell1 & Hmodel'1) (%_l & %_t' & %Heq & Hcell2 & Hmodel'2)". injection Heq as <-.
+      simpl in Hlength. eapply le_S_n in Hlength.
+      iDestruct (record_three_model_combine with "Hcell1 Hcell2") as "(Hcell & _ & <- & <-)".
+      iDestruct ("IH" with "[] Hmodel'1 Hmodel'2") as "(Hmodel' & Hmodel'2 & ->)"; first done.
+      iFrame. iSplit; last rewrite /= take_length min_l //.
+      iExists l, t'. auto with iFrame.
+  Qed.
+  Lemma dlchain_model_combine' t prev dq1 vs1 last1 next1 dq2 vs2 last2 next2 :
+    length vs1 = length vs2 →
+    dlchain_model t dq1 prev vs1 last1 next1 -∗
+    dlchain_model t dq2 prev vs2 last2 next2 -∗
+      dlchain_model t (dq1 ⋅ dq2) prev vs1 last1 next1 ∗
+      ⌜vs1 = vs2 ∧ last1 = last2 ∧ next1 = next2⌝.
+  Proof.
+    iIntros "%Hlength Hmodel1 Hmodel2".
+    iDestruct (dlchain_model_combine with "Hmodel1 Hmodel2") as "($ & Hmodel2 & ->)"; first lia.
+    rewrite Hlength take_length min_l; last lia.
+    rewrite drop_all. iDestruct "Hmodel2" as %(-> & ->).
+    rewrite take_ge //.
+  Qed.
+  Lemma dlchain_model_valid_2 t prev dq1 vs1 last1 next1 dq2 vs2 last2 next2 :
+    0 < length vs1 ≤ length vs2 →
+    dlchain_model t dq1 prev vs1 last1 next1 -∗
+    dlchain_model t dq2 prev vs2 last2 next2 -∗
+    ⌜✓ (dq1 ⋅ dq2) ∧ vs1 = take (length vs1) vs2⌝.
+  Proof.
+    iIntros "% Hmodel1 Hmodel2".
+    iDestruct (dlchain_model_combine with "Hmodel1 Hmodel2") as "(Hmodel & _ & %)"; first lia.
+    iDestruct (dlchain_model_valid with "Hmodel") as %?; first lia.
+    done.
+  Qed.
+  Lemma dlchain_model_valid_2' t prev dq1 vs1 last1 next1 dq2 vs2 last2 next2 :
+    0 < length vs1 →
+    length vs1 = length vs2 →
+    dlchain_model t dq1 prev vs1 last1 next1 -∗
+    dlchain_model t dq2 prev vs2 last2 next2 -∗
+    ⌜✓ (dq1 ⋅ dq2) ∧ vs1 = vs2 ∧ last1 = last2 ∧ next1 = next2⌝.
+  Proof.
+    iIntros "% % Hmodel1 Hmodel2".
+    iDestruct (dlchain_model_combine' with "Hmodel1 Hmodel2") as "(Hmodel & <- & <- & <-)"; first done.
+    iDestruct (dlchain_model_valid with "Hmodel") as %?; done.
+  Qed.
+  Lemma dlchain_model_agree t prev dq1 vs1 last1 next1 dq2 vs2 last2 next2 :
+    length vs1 = length vs2 →
+    dlchain_model t dq1 prev vs1 last1 next1 -∗
+    dlchain_model t dq2 prev vs2 last2 next2 -∗
+    ⌜vs1 = vs2 ∧ last1 = last2 ∧ next1 = next2⌝.
+  Proof.
+    iIntros "% Hmodel1 Hmodel2".
+    iDestruct (dlchain_model_combine' with "Hmodel1 Hmodel2") as "(_ & <- & <- & <-)"; done.
+  Qed.
+  Lemma dlchain_model_dfrac_ne prev t1 dq1 vs1 last1 next1 t2 dq2 vs2 last2 next2 :
+    0 < length vs1 ≤ length vs2 →
+    ¬ ✓ (dq1 ⋅ dq2) →
+    dlchain_model t1 dq1 prev vs1 last1 next1 -∗
+    dlchain_model t2 dq2 prev vs2 last2 next2 -∗
+    ⌜t1 ≠ t2⌝.
+  Proof.
+    iIntros "% % Hmodel1 Hmodel2" (->).
+    iDestruct (dlchain_model_valid_2 with "Hmodel1 Hmodel2") as %?; naive_solver.
+  Qed.
+  Lemma dlchain_model_ne prev t1 vs1 last1 next1 t2 dq2 vs2 last2 next2 :
+    0 < length vs1 ≤ length vs2 →
+    dlchain_model t1 (DfracOwn 1) prev vs1 last1 next1 -∗
+    dlchain_model t2 dq2 prev vs2 last2 next2 -∗
+    ⌜t1 ≠ t2⌝.
+  Proof.
+    intros. iApply dlchain_model_dfrac_ne; [done | intros []%exclusive_l; apply _].
+  Qed.
+  Lemma dlchain_model_exclusive t prev vs1 last1 next1 vs2 last2 next2 :
+    0 < length vs1 ≤ length vs2 →
+    dlchain_model t (DfracOwn 1) prev vs1 last1 next1 -∗
+    dlchain_model t (DfracOwn 1) prev vs2 last2 next2 -∗
+    False.
+  Proof.
+    iIntros "% Hmodel1 Hmodel2".
+    iDestruct (dlchain_model_ne with "Hmodel1 Hmodel2") as %?; naive_solver.
+  Qed.
+
+  Lemma dlchain_prev_spec t dq prev vs last next :
+    0 < length vs →
+    {{{ dlchain_model t dq prev vs last next }}}
+      dlchain_prev t
+    {{{ RET prev; dlchain_model t dq prev vs last next }}}.
+  Proof.
+    destruct vs as [| v vs]; [naive_solver lia | intros _].
+    iIntros "%Φ (%l & %t' & -> & Hcell & Hmodel') HΦ".
+    wp_rec. wp_pures.
+    wp_apply (record_three_get0_spec with "Hcell"). iIntros "Hcell".
+    iApply "HΦ". iExists l, t'. auto with iFrame.
+  Qed.
+  Lemma dlchain_data_spec t dq prev v vs last next :
+    {{{ dlchain_model t dq prev (v :: vs) last next }}}
+      dlchain_data t
+    {{{ RET v; dlchain_model t dq prev (v :: vs) last next }}}.
+  Proof.
+    iIntros "%Φ (%l & %t' & -> & Hcell & Hmodel') HΦ".
+    wp_rec. wp_pures.
+    wp_apply (record_three_get1_spec with "Hcell"). iIntros "Hcell".
+    iApply "HΦ". iFrame. iExists l, t'. auto with iFrame.
+  Qed.
+  Lemma dlchain_next_spec t dq prev v vs last next :
+    {{{ dlchain_model t dq prev (v :: vs) last next }}}
+      dlchain_next t
+    {{{ t',
+      RET t';
+      dlchain_model t dq prev [v] t t' ∗
+      dlchain_model t' dq t vs last next
+    }}}.
+  Proof.
+    iIntros "%Φ (%l & %t' & -> & Hcell & Hmodel') HΦ".
+    wp_rec. wp_pures.
+    wp_apply (record_three_get2_spec with "Hcell"). iIntros "Hcell".
+    iApply "HΦ". iFrame. iExists l, t'. auto with iFrame.
+  Qed.
+
+  Lemma dlchain_set_prev_spec t prev vs last next prev' :
+    0 < length vs →
+    {{{ dlchain_model t (DfracOwn 1) prev vs last next }}}
+      dlchain_set_prev t prev'
+    {{{ RET #(); dlchain_model t (DfracOwn 1) prev' vs last next }}}.
+  Proof.
+    destruct vs as [| v vs]; [naive_solver lia | intros _].
+    iIntros "%Φ (%l & %t' & -> & Hcell & Hmodel') HΦ".
+    wp_rec. wp_pures.
+    wp_apply (record_three_set0_spec with "Hcell"). iIntros "Hcell".
+    iApply "HΦ". iExists l, t'. auto with iFrame.
+  Qed.
+  Lemma dlchain_set_data_spec t prev v vs last next w :
+    {{{ dlchain_model t (DfracOwn 1) prev (v :: vs) last next }}}
+      dlchain_set_data t w
+    {{{ RET #(); dlchain_model t (DfracOwn 1) prev (w :: vs) last next }}}.
+  Proof.
+    iIntros "%Φ (%l & %t' & -> & Hcell & Hmodel') HΦ".
+    wp_rec. wp_pures.
+    wp_apply (record_three_set1_spec with "Hcell"). iIntros "Hcell".
+    iApply "HΦ". iExists l, t'. auto with iFrame.
+  Qed.
+  Lemma dlchain_set_next_spec t prev v vs last next w :
+    {{{ dlchain_model t (DfracOwn 1) prev (v :: vs) last next }}}
+      dlchain_set_next t w
+    {{{ t',
+      RET #();
+      dlchain_model t (DfracOwn 1) prev [v] t w ∗
+      dlchain_model t' (DfracOwn 1) t vs last next
+    }}}.
+  Proof.
+    iIntros "%Φ (%l & %t' & -> & Hcell & Hmodel') HΦ".
+    wp_rec. wp_pures.
+    wp_apply (record_three_set2_spec with "Hcell"). iIntros "Hcell".
+    iApply "HΦ". iFrame. iExists l, w. auto with iFrame.
+  Qed.
+
+  Lemma dlchain_singleton_spec prev v next :
+    {{{ True }}}
+      dlchain_singleton prev v next
+    {{{ t, RET t; dlchain_model t (DfracOwn 1) prev [v] t next }}}.
+  Proof.
+    iIntros "%Φ _ HΦ".
+    wp_rec. wp_pures.
+    wp_apply (record_three_make_spec with "[//]"). iIntros "%l Hl".
+    iApply "HΦ". iExists l, next. auto with iFrame.
+  Qed.
+End heapGS.
+
+#[global] Opaque dlchain_prev.
+#[global] Opaque dlchain_data.
+#[global] Opaque dlchain_next.
+#[global] Opaque dlchain_set_prev.
+#[global] Opaque dlchain_set_data.
+#[global] Opaque dlchain_set_next.
+#[global] Opaque dlchain_singleton.
+#[global] Opaque dlchain_advance_backward.
+#[global] Opaque dlchain_advance_forward.
+#[global] Opaque dlchain_advance.
+#[global] Opaque dlchain_get.
+#[global] Opaque dlchain_set.
+
+#[global] Opaque dlchain_model.
