c4012802b31f02f2132b333e0f73546ac948b59e
clallain <clement.allain@inria.fr>
2023-03-27 19:29:54 +0200

    Add lang/identifier.

---
+++ theories/lang/identifier.v
@@ -0,0 +1,50 @@
+From caml5 Require Import
+  prelude.
+From caml5.lang Require Export
+  lang.
+From caml5.lang Require Import
+  notations
+  proofmode.
+
+Definition identifier :=
+  proph_id.
+Canonical identifier_O :=
+  leibnizO identifier.
+Implicit Types id : identifier.
+
+Definition LitIdentifier id :=
+  LitProphecy id.
+Coercion LitIdentifier : identifier >-> base_lit.
+
+Notation NewId := NewProph
+( only parsing
+).
+
+Section heapGS.
+  Context `{!heapGS Σ}.
+
+  Definition identifier_model id : iProp Σ :=
+    ∃ prophs, proph id prophs.
+
+  Lemma identifier_exclusive id :
+    identifier_model id -∗
+    identifier_model id -∗
+    False.
+  Proof.
+    iIntros "(% & Hid1) (% & Hid2)". iApply (proph_exclusive with "Hid1 Hid2").
+  Qed.
+
+  Lemma wp_new_id E :
+    {{{ True }}}
+      NewId @ E
+    {{{ id, RET #id; identifier_model id }}}.
+  Proof.
+    iIntros "%Φ _ HΦ".
+    wp_apply (wp_new_proph with "[//]"). iIntros "%prophs %p Hp".
+    iApply "HΦ". iExists prophs. done.
+  Qed.
+End heapGS.
+
+#[global] Opaque LitIdentifier.
+
+#[global] Opaque identifier_model.
