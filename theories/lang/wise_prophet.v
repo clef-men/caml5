f06b15370e04f8c755995a69ca8466eedbdc6e95
clallain <clement.allain@inria.fr>
2023-04-04 14:47:12 +0200

    Redesign lang/wise_prophet.

--- theories/lang/wise_prophet.v
+++ theories/lang/wise_prophet.v
@@ -1,7 +1,6 @@
 From caml5 Require Import
   prelude.
 From caml5.base_logic Require Import
-  lib.auth_excl
   lib.agree
   lib.mono_list.
 From caml5.lang Require Import
@@ -11,16 +10,13 @@ From caml5.lang Require Export
   typed_prophet.
 
 Class WiseProphetG Σ `{!heapGS Σ} prophet := {
-  wise_prophet_G_model_G : AuthExclG Σ $ leibnizO (list prophet.(typed_prophet_type)) ;
   wise_prophet_G_full_G : AgreeG Σ $ leibnizO (list prophet.(typed_prophet_type)) ;
   wise_prophet_G_past_G : mono_listG prophet.(typed_prophet_type) Σ ;
 }.
-#[local] Existing Instance wise_prophet_G_model_G.
 #[local] Existing Instance wise_prophet_G_full_G.
 #[local] Existing Instance wise_prophet_G_past_G.
 
 Definition wise_prophet_Σ prophet := #[
-  auth_excl_Σ $ leibnizO (list prophet.(typed_prophet_type)) ;
   agree_Σ $ leibnizO (list prophet.(typed_prophet_type)) ;
   mono_listΣ prophet.(typed_prophet_type)
 ].
@@ -32,206 +28,128 @@ Proof.
 Qed.
 
 Section wise_prophet.
-  Context `{!heapGS Σ} (prophet : typed_prophet) `{!WiseProphetG Σ prophet}.
+  Context `{!heapGS Σ} prophet `{!WiseProphetG Σ prophet}.
 
-  Definition wise_prophet_name := gname.
+  Definition wise_prophet_name : Type := gname * gname.
+
+  #[global] Instance wise_prophet_name_eq_dec :
+    EqDecision wise_prophet_name.
+  Proof.
+    apply _.
+  Defined.
+  #[global] Instance wise_prophet_name_countable :
+    Countable wise_prophet_name.
+  Proof.
+    apply _.
+  Qed.
 
   Notation prophecies := (list prophet.(typed_prophet_type)).
   Let prophecies_O := leibnizO prophecies.
   #[local] Canonical prophecies_O.
 
-  Implicit Types γ_model : wise_prophet_name.
-  Implicit Types γ_full γ_past : gname.
+  Implicit Types γ : wise_prophet_name.
   Implicit Types prophs : prophecies.
 
-  Definition wise_prophet_full γ_full full :=
-    agree_on γ_full full.
+  Notation "γ '.(full)'" := γ.1
+  ( at level 5
+  ) : stdpp_scope.
+  Notation "γ '.(past)'" := γ.2
+  ( at level 5
+  ) : stdpp_scope.
 
-  Definition wise_prophet_past_auth γ_past past :=
-    mono_list_auth γ_past 1 past.
-  Definition wise_prophet_past_lb γ_past past_lb :=
-    mono_list_lb γ_past past_lb.
+  Definition wise_prophet_model p γ past prophs : iProp Σ :=
+    agree_on γ.(full) (past ++ prophs) ∗
+    mono_list_auth γ.(past) 1 past ∗
+    typed_prophet_model prophet p prophs.
 
-  Definition wise_prophet_model γ_model prophs :=
-    auth_excl_auth γ_model (DfracOwn 1) prophs.
-
-  #[local] Definition wise_prophet_inv_inner p γ_model γ_full γ_past : iProp Σ :=
-    ∃ past prophs,
-    typed_prophet_model prophet p prophs ∗
-    auth_excl_frag γ_model prophs ∗
-    wise_prophet_full γ_full (past ++ prophs) ∗
-    wise_prophet_past_auth γ_past past.
-  Definition wise_prophet_inv p γ_model γ_full γ_past ι :=
-    inv ι (wise_prophet_inv_inner p γ_model γ_full γ_past).
-
-  Definition wise_prophet_lb γ_full γ_past lb : iProp Σ :=
+  Definition wise_prophet_lb γ lb : iProp Σ :=
     ∃ past_lb,
-    wise_prophet_full γ_full (past_lb ++ lb) ∗
-    wise_prophet_past_lb γ_past past_lb.
+    agree_on γ.(full) (past_lb ++ lb) ∗
+    mono_list_lb γ.(past) past_lb.
 
-  #[global] Instance wise_prophet_model_timeless γ_model prophs :
-    Timeless (wise_prophet_model γ_model prophs).
+  #[global] Instance wise_prophet_model_timeless p γ past prophs :
+    Timeless (wise_prophet_model p γ past prophs).
   Proof.
     apply _.
   Qed.
-  #[global] Instance wise_prophet_inv_persistent p γ_model γ_full γ_past ι :
-    Persistent (wise_prophet_inv p γ_model γ_full γ_past ι).
+  #[global] Instance wise_prophet_lb_timeless γ lb :
+    Timeless (wise_prophet_lb γ lb).
   Proof.
     apply _.
   Qed.
-  #[global] Instance wise_prophet_lb_timeless γ_full γ_past lb :
-    Timeless (wise_prophet_lb γ_full γ_past lb).
-  Proof.
-    apply _.
-  Qed.
-  #[global] Instance wise_prophet_lb_persistent γ_full γ_past lb :
-    Persistent (wise_prophet_lb γ_full γ_past lb).
+  #[global] Instance wise_prophet_lb_persistent γ lb :
+    Persistent (wise_prophet_lb γ lb).
   Proof.
     apply _.
   Qed.
 
-  Lemma wise_prophet_model_exclusive p prophs1 prophs2 :
-    wise_prophet_model p prophs1 -∗
-    wise_prophet_model p prophs2 -∗
+  Lemma wise_prophet_model_exclusive p γ1 past1 prophs1 γ2 past2 prophs2 :
+    wise_prophet_model p γ1 past1 prophs1 -∗
+    wise_prophet_model p γ2 past2 prophs2 -∗
     False.
   Proof.
-    apply auth_excl_auth_exclusive.
+    iIntros "(_ & _ & Hmodel1) (_ & _ & Hmodel2)".
+    iApply (typed_prophet_model_exclusive with "Hmodel1 Hmodel2").
   Qed.
 
-  Lemma wise_prophet_inv_acc p γ_model γ_full γ_past ι E :
-    ↑ι ⊆ E →
-    wise_prophet_inv p γ_model γ_full γ_past ι ={E, E ∖ ↑ι}=∗
-      ∃ past prophs,
-      wise_prophet_full γ_full (past ++ prophs) ∗
-      wise_prophet_past_auth γ_past past ∗
-      (wise_prophet_past_auth γ_past past ={E ∖ ↑ι, E}=∗ True).
+  Lemma wise_prophet_lb_get p γ past prophs :
+    wise_prophet_model p γ past prophs -∗
+    wise_prophet_lb γ prophs.
   Proof.
-    iIntros "% #Hinv".
-    iMod (inv_acc with "Hinv") as "((%past & %prophs & >Hp & >Hmodel₂ & >#Hfull & >Hpast_auth) & Hclose)"; first done.
-    iModIntro. iExists past, prophs. iFrame "#∗". iIntros "Hpast_auth".
-    iApply "Hclose". iNext. iExists past, prophs. iFrame "#∗".
+    iIntros "(#Hfull & Hpast_auth & Hmodel)".
+    iExists past. iFrame "#∗". iApply (mono_list_lb_get with "Hpast_auth").
   Qed.
-
-  Lemma wise_prophet_lb_get γ_full γ_past past prophs :
-    wise_prophet_full γ_full (past ++ prophs) -∗
-    wise_prophet_past_auth γ_past past -∗
-    wise_prophet_lb γ_full γ_past prophs.
+  Lemma wise_prophet_model_lb_valid p γ past prophs lb :
+    wise_prophet_model p γ past prophs -∗
+    wise_prophet_lb γ lb -∗
+    ⌜∃ past1 past2, past = past1 ++ past2 ∧ lb = past2 ++ prophs⌝.
   Proof.
-    iIntros "Hfull Hpast_auth".
-    iExists past. iFrame. iApply (mono_list_lb_get with "Hpast_auth").
-  Qed.
-  Lemma wise_prophet_lb_get' p γ_model γ_full γ_past ι prophs E :
-    ↑ι ⊆ E →
-    wise_prophet_inv p γ_model γ_full γ_past ι -∗
-    wise_prophet_model γ_model prophs ={E}=∗
-      wise_prophet_model γ_model prophs ∗
-      wise_prophet_lb γ_full γ_past prophs.
-  Proof.
-    iIntros "% #Hinv Hmodel₁".
-    iInv "Hinv" as "(%past & %_prophs & >Hp & >Hmodel₂ & >#Hfull & >Hpast_auth)".
-    iDestruct (auth_excl_agree_L with "Hmodel₁ Hmodel₂") as %<-.
-    iDestruct (wise_prophet_lb_get with "Hfull Hpast_auth") as "#$".
-    iModIntro. iFrame. iSplitL; last done. iExists past, prophs. iFrame "#∗".
-  Qed.
-  Lemma wise_prophet_lb_suffix γ_full γ_past past prophs lb :
-    wise_prophet_full γ_full (past ++ prophs) -∗
-    wise_prophet_past_auth γ_past past -∗
-    wise_prophet_lb γ_full γ_past lb -∗
-    ⌜prophs `suffix_of` lb⌝.
-  Proof.
-    iIntros "Hfull Hpast_auth (%past_lb & Hfull' & Hpast_lb)".
+    iIntros "(#Hfull & Hpast_auth & Hmodel) (%past1 & #Hfull' & #Hpast_lb)".
     iDestruct (agree_on_agree_L with "Hfull Hfull'") as %Hfull.
-    iDestruct (mono_list_auth_lb_valid with "Hpast_auth Hpast_lb") as %(_ & past_delta & ->).
-    iPureIntro. rewrite -assoc in Hfull. solve_suffix.
-  Qed.
-  Lemma wise_prophet_lb_suffix' p γ_model γ_full γ_past ι prophs lb E :
-    ↑ι ⊆ E →
-    wise_prophet_inv p γ_model γ_full γ_past ι -∗
-    wise_prophet_model γ_model prophs -∗
-    wise_prophet_lb γ_full γ_past lb ={E}=∗
-      wise_prophet_model γ_model prophs ∗
-      ⌜prophs `suffix_of` lb⌝.
-  Proof.
-    iIntros "% #Hinv Hmodel₁ Hlb".
-    iInv "Hinv" as "(%past & %_prophs & >Hp & >Hmodel₂ & >#Hfull & >Hpast_auth)".
-    iDestruct (auth_excl_agree_L with "Hmodel₁ Hmodel₂") as %<-.
-    iDestruct (wise_prophet_lb_suffix with "Hfull Hpast_auth Hlb") as "#$".
-    iModIntro. iFrame. iSplitL; last done. iExists past, prophs. iFrame "#∗".
+    iDestruct (mono_list_auth_lb_valid with "Hpast_auth Hpast_lb") as %(_ & past2 & ->).
+    iExists past1, past2. rewrite -assoc in Hfull. naive_solver.
   Qed.
 
-  Lemma wise_prophet_new_proph_spec ι E :
+  Lemma wise_prophet_new_proph_spec E :
     {{{ True }}}
       NewProph @ E
-    {{{ p γ_model γ_full γ_past prophs,
+    {{{ p γ prophs,
       RET #p;
-      wise_prophet_inv p γ_model γ_full γ_past ι ∗
-      wise_prophet_model γ_model prophs
+      wise_prophet_model p γ [] prophs
     }}}.
   Proof.
     iIntros "%Φ _ HΦ".
     iApply wp_fupd. wp_apply (typed_prophet_new_proph_spec with "[//]"). iIntros "%p %prophs Hp".
-    iMod (auth_excl_alloc' prophs) as "(%γ_model & Hmodel₁ & Hmodel₂)".
     iMod (agree_alloc prophs) as "(%γ_full & #Hfull)".
     iMod (mono_list_alloc []) as "(%γ_past & Hpast_auth & _)".
-    iApply "HΦ". iFrame. iApply inv_alloc. iNext. iExists [], prophs. auto with iFrame.
+    iApply ("HΦ" $! _ (γ_full, γ_past)). iFrame "#∗". done.
   Qed.
 
-  Lemma wise_prophet_resolve_spec e v E p γ_model γ_full γ_past ι prophs Φ :
-    ↑ι ⊆ E →
+  Lemma wise_prophet_resolve_spec e v E p γ past prophs Φ :
     Atomic StronglyAtomic e →
     to_val e = None →
-    wise_prophet_inv p γ_model γ_full γ_past ι -∗
-    wise_prophet_model γ_model prophs -∗
-    WP e @ E ∖ ↑ι {{ w,
-      ∀ past,
-      wise_prophet_full γ_full (past ++ prophs) -∗
-      wise_prophet_past_auth γ_past past ={E ∖ ↑ι}=∗
-        ∃ proph,
-        ⌜(w, v) = prophet.(typed_prophet_to_val) proph⌝ ∗
-        wise_prophet_past_auth γ_past past ∗
-          ∀ prophs',
-          ⌜prophs = proph :: prophs'⌝ -∗
-          wise_prophet_model γ_model prophs' ={E ∖ ↑ι}=∗
-          Φ w
-    }} -∗
-    WP Resolve e #p v @ E {{ Φ }}.
-  Proof.
-    iIntros "% % % Hinv Hmodel₁ HΦ".
-    iInv "Hinv" as "(%past & %_prophs & >Hp & >Hmodel₂ & >#Hfull & >Hpast_auth)"; first apply strongly_atomic_atomic, _.
-    iDestruct (auth_excl_agree_L with "Hmodel₁ Hmodel₂") as %<-.
-    wp_apply (typed_prophet_resolve_spec with "Hp"); first done.
-    iApply wp_fupd. wp_apply (wp_wand with "HΦ"). iIntros "%w HΦ".
-    iMod ("HΦ" with "Hfull Hpast_auth") as "(%proph & % & Hpast_auth & HΦ)".
-    iModIntro. iExists proph. iSplitR; first done. iIntros "%prophs' -> Hp".
-    iMod (auth_excl_update' prophs' with "Hmodel₁ Hmodel₂") as "(Hmodel₁ & Hmodel₂)".
-    iMod (mono_list_auth_update_app [proph] with "Hpast_auth") as "(Hpast_auth & _)".
-    iMod ("HΦ" with "[//] Hmodel₁") as "$".
-    iModIntro. iNext. iExists (past ++ [proph]), prophs'. iFrame. list_simplifier. done.
-  Qed.
-  Lemma wise_prophet_resolve_spec' e v E p γ_model γ_full γ_past ι prophs Φ :
-    ↑ι ⊆ E →
-    Atomic StronglyAtomic e →
-    to_val e = None →
-    wise_prophet_inv p γ_model γ_full γ_past ι -∗
-    wise_prophet_model γ_model prophs -∗
-    WP e @ E ∖ ↑ι {{ w,
+    wise_prophet_model p γ past prophs -∗
+    WP e @ E {{ w,
       ∃ proph,
       ⌜(w, v) = prophet.(typed_prophet_to_val) proph⌝ ∗
         ∀ prophs',
         ⌜prophs = proph :: prophs'⌝ -∗
-        wise_prophet_model γ_model prophs' ={E ∖ ↑ι}=∗
+        wise_prophet_model p γ (past ++ [proph]) prophs' -∗
         Φ w
     }} -∗
     WP Resolve e #p v @ E {{ Φ }}.
   Proof.
-    iIntros "% % % Hinv Hmodel₁ HΦ".
-    wp_apply (wise_prophet_resolve_spec with "Hinv Hmodel₁"); [done.. |].
-    wp_apply (wp_wand with "HΦ"). iIntros "%w (%proph & % & HΦ) %past _ Hpast_auth".
-    iModIntro. iExists proph. iFrame. done.
+    iIntros "% % (#Hfull & Hpast_auth & Hmodel) HΦ".
+    wp_apply (typed_prophet_resolve_spec with "Hmodel"); first done.
+    iApply wp_fupd. wp_apply (wp_wand with "HΦ"). iIntros "%w HΦ".
+    iDestruct "HΦ" as "(%proph & % & HΦ)".
+    iExists proph. iSplitR; first done.
+    iMod (mono_list_auth_update_app [proph] with "Hpast_auth") as "(Hpast_auth & _)".
+    iIntros "!> %prophs' -> Hp".
+    iApply ("HΦ" with "[//]"). iFrame. list_simplifier. done.
   Qed.
 End wise_prophet.
 
 #[global] Opaque wise_prophet_name.
 #[global] Opaque wise_prophet_model.
-#[global] Opaque wise_prophet_inv.
 #[global] Opaque wise_prophet_lb.
