7cf3dc556b638b77036927e3d4f5fc0d8ddd302a
clallain <clement.allain@inria.fr>
2023-05-12 22:00:12 +0200

    Rewriting.

--- theories/concurrent/condition.v
+++ theories/concurrent/condition.v
@@ -20,22 +20,39 @@ Record condition `{!heapGS Σ} {mutex_unboxed} {mutex : mutex Σ mutex_unboxed} 
   condition_make_spec :
     {{{ True }}}
       condition_make #()
-    {{{ t, RET t; condition_inv t }}} ;
+    {{{ t,
+      RET t; condition_inv t
+    }}} ;
 
   condition_wait_spec t mtx P :
-    {{{ condition_inv t ∗ mutex.(mutex_inv) mtx P ∗ mutex.(mutex_locked) mtx ∗ P }}}
+    {{{
+      condition_inv t ∗
+      mutex.(mutex_inv) mtx P ∗
+      mutex.(mutex_locked) mtx ∗ P
+    }}}
       condition_wait t mtx
-    {{{ RET #(); mutex.(mutex_locked) mtx ∗ P }}} ;
+    {{{
+      RET #();
+      mutex.(mutex_locked) mtx ∗ P
+    }}} ;
 
   condition_signal_spec t :
-    {{{ condition_inv t }}}
+    {{{
+      condition_inv t
+    }}}
       condition_signal t
-    {{{ RET #(); True }}} ;
+    {{{
+      RET #(); True
+    }}} ;
 
   condition_broadcast_spec t :
-    {{{ condition_inv t }}}
+    {{{
+      condition_inv t
+    }}}
       condition_broadcast t
-    {{{ RET #(); True }}} ;
+    {{{
+      RET #(); True
+    }}} ;
 
   condition_unboxed :
     if unboxed then ∀ t,
@@ -52,8 +69,9 @@ Section condition.
   Context `{!heapGS Σ}.
   Context {mutex_unboxed} {mutex : mutex Σ mutex_unboxed}.
   Context {unboxed} (condition : condition Σ mutex unboxed).
+  Implicit Types cond : val.
 
-  #[local] Definition condition_wait_until_aux (cond : val) : val :=
+  #[local] Definition condition_wait_until_aux cond : val :=
     rec: "condition_wait_until_aux" "t" "mtx" :=
       if: cond #() then #() else (
         condition.(condition_wait) "t" "mtx" ;;
@@ -63,28 +81,38 @@ Section condition.
     λ: "t" "mtx",
       condition_wait_until_aux cond "t" "mtx".
 
-  Definition condition_wait_while (cond : val) :=
+  Definition condition_wait_while cond :=
     condition_wait_until (λ: <>, ~ cond #()).
 
-  Lemma condition_wait_until_spec (cond : val) t mtx P Φ :
+  Lemma condition_wait_until_spec cond t mtx P Φ :
     {{{
-      condition.(condition_inv) t ∗ mutex.(mutex_inv) mtx P ∗
-      mutex.(mutex_locked) mtx ∗ P ∗ Φ false ∗
-      {{{ mutex.(mutex_locked) mtx ∗ P ∗ Φ false }}}
+      condition.(condition_inv) t ∗
+      mutex.(mutex_inv) mtx P ∗
+      mutex.(mutex_locked) mtx ∗ P ∗
+      Φ false ∗
+      {{{
+        mutex.(mutex_locked) mtx ∗ P ∗
+        Φ false
+      }}}
         cond #()
-      {{{ (b : bool), RET #b; mutex.(mutex_locked) mtx ∗ P ∗ Φ b }}}
+      {{{ (b : bool),
+        RET #b;
+        mutex.(mutex_locked) mtx ∗ P ∗
+        Φ b
+      }}}
     }}}
       condition_wait_until cond t mtx
     {{{
       RET #();
-      mutex.(mutex_locked) mtx ∗ P ∗ Φ true
+      mutex.(mutex_locked) mtx ∗ P ∗
+      Φ true
     }}}.
   Proof.
     iIntros "%Ψ (#Hinv_t & #Hinv_mtx & Hlocked & HP & HΦ & #Hcond) HΨ".
     wp_rec. wp_pures.
     iLöb as "HLöb".
-    wp_rec. wp_pures.
-    wp_apply ("Hcond" with "[$]"). iIntros "%b (Hlocked & HP & HΦ)".
+    wp_rec.
+    wp_smart_apply ("Hcond" with "[$]"). iIntros "%b (Hlocked & HP & HΦ)".
     destruct b; wp_pures.
     { iApply "HΨ". iFrame. done. }
     wp_apply (condition_wait_spec _  _ _ P with "[$]"). iIntros "(Hlocked & HP)".
@@ -92,28 +120,44 @@ Section condition.
     iApply ("HLöb" with "[$] [$] [$] [$]").
   Qed.
 
-  Lemma condition_wait_while_spec (cond : val) t mtx P Φ :
+  Lemma condition_wait_while_spec cond t mtx P Φ :
     {{{
-      condition.(condition_inv) t ∗ mutex.(mutex_inv) mtx P ∗
-      mutex.(mutex_locked) mtx ∗ P ∗ Φ true ∗
-      {{{ mutex.(mutex_locked) mtx ∗ P ∗ Φ true }}}
+      condition.(condition_inv) t ∗
+      mutex.(mutex_inv) mtx P ∗
+      mutex.(mutex_locked) mtx ∗ P ∗
+      Φ true ∗
+      {{{
+        mutex.(mutex_locked) mtx ∗ P ∗
+        Φ true
+      }}}
         cond #()
-      {{{ (b : bool), RET #b; mutex.(mutex_locked) mtx ∗ P ∗ Φ b }}}
+      {{{ (b : bool),
+        RET #b;
+        mutex.(mutex_locked) mtx ∗ P ∗
+        Φ b
+      }}}
     }}}
       condition_wait_while cond t mtx
     {{{
       RET #();
-      mutex.(mutex_locked) mtx ∗ P ∗ Φ false
+      mutex.(mutex_locked) mtx ∗ P ∗
+      Φ false
     }}}.
   Proof.
     iIntros "%Ψ (#Hinv_t & #Hinv_mtx & Hlocked & HP & HΦ & #Hcond) HΨ".
     wp_apply (condition_wait_until_spec _ _ _ P (λ b, Φ (negb b)) with "[$Hlocked $HP $HΦ]"); last done.
     iFrame "#". clear. iIntros "%Ψ !> (Hlocked & HP & HΦ) HΨ".
-    wp_pures.
-    wp_apply ("Hcond" with "[$]"). iIntros "%b (Hlocked & HP & HΦ)".
+    wp_smart_apply ("Hcond" with "[$]"). iIntros "%b (Hlocked & HP & HΦ)".
     destruct b; wp_pures; iApply "HΨ"; iFrame; done.
   Qed.
 End condition.
 
 #[global] Opaque condition_wait_until.
 #[global] Opaque condition_wait_while.
+
+Notation "condition .(condition_wait_until)" := (condition_wait_until condition)
+( at level 5
+).
+Notation "condition .(condition_wait_while)" := (condition_wait_while condition)
+( at level 5
+).
